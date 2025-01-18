7152603fca3d477b2f2f73e25bd20c754a22c1ca
Joey-zfp <126837609+Joey-zfp@users.noreply.github.com>
2025-01-18 23:40:01 +0800

    app.py

--- basic/app.py
+++ basic/app.py
@@ -42,4 +42,5 @@ def get_response():
     return jsonify({'response': answer})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))  # 讀取 Render 自動分配的 PORT
+    app.run(host='0.0.0.0', port=port)
